57cdc641f8200d8baef74e265dc873ed845aec30
giovanijfc <giovanijfc@gmail.com>
2019-02-15 17:04:54 -0200

    Salvando perfis de usuario na base de dados

--- src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -24,12 +24,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private Environment env;
 
 	private static final String[] PUBLIC_MATCHERS = {
-			"/h2-console/**"
+			"/h2/**"
 	};
 
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
-			"/categorias/**"
+			"/categorias/**",
+			"/clientes/**"
 	};
 
 	@Override
